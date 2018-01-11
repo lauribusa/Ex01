0c6e55f0f55c11c9eea7bbacc199453f713a0d99
Lightbolt <lauri.busa@gmail.com>
2018-01-11 11:58:40 +0100

    doubler une array

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("Kekchose");
+var obj = {
+    name:'ta mère',
+    job:'consternant',
+    toString:function(){
+        return `le nom est ${this.name} et job: ${this.job}`
+    }
+}
+
+const number = [1,2,3,4,5,6];
+
+function dblnumbers(arr){
+    let newarr = [];
+    arr.forEach(function(element) {
+    return newarr.push(element * 2);
+      });
+    console.log(newarr);
+}
+dblnumbers(number);
+
+console.log(obj.toString());
